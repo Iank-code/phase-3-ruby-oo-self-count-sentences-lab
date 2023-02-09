07a1e52d5a26c756a9f44d7e73790fee87dea52f
ianK-code <kian99564@gmail.com>
2023-02-09 14:01:40 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # return true if self.end_with?(".")
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self == (nil || "") ? 0 : self.split(/[.?!]+/).count #(self.split(" ").count)
+    # self.split(/\.\s/).count
+    # self.length
   end
 end
